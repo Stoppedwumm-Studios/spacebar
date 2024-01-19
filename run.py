04a62c7ad20b9008168fb0529860a9dd6aed8f7c
StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>
2024-01-19 12:58:43 +0100

    Open Link Button
    Signed-off-by: StoppedwummSites <150438484+StoppedwummSites@users.noreply.github.com>

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 from pyautogui import press, typewrite, hotkey
 import time
 import tkinter as tk
+import os
 
 print("Made by Stoppedwumm-Studios")
 
 print("https://github.com/Stoppedwumm-Studios/spacebar")
+
+def OpenClick():
+    os.system("run https://github.com/Stoppedwumm-Studios/spacebar")
 
 def Click():
     clicks = 0
@@ -20,10 +24,12 @@ greeting = tk.Label(window, text="Click 120 Times")
 schaltf1 = tk.Button(window, text="Do it (:<", command=Click)
 credit = tk.Label(window, text="Made by Stoppedwumm-Studios")
 site = tk.Label(window, text="https://github.com/Stoppedwumm-Studios/spacebar/")
+opensite = tk.Button(window, text="Open Page (Only Windows)", command=OpenClick)
 greeting.pack()
 schaltf1.pack()
 credit.pack()
 site.pack()
+opensite.pack()
 print("Window prepared")
 window.mainloop()
 
