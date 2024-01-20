ce81e8ece8f2b6f6fff6d8a0ea5ccfd4aaeafc54
Stoppedwumm <129097720+Stoppedwumm@users.noreply.github.com>
2024-01-20 12:32:21 +0100

    Rename thread variable
    Signed-off-by: Stoppedwumm <129097720+Stoppedwumm@users.noreply.github.com>

--- app.py
+++ app.py
@@ -19,12 +19,12 @@ def Click():
         time.sleep(0.00001)
         print("CLICK " + str(clicks))
 
-x = threading.Thread(target=Click, daemon=True)
+clickthread = threading.Thread(target=Click, daemon=True)
 
 print("Defined Clicking")
 window = tk.Tk()
 greeting = tk.Label(window, text="Click 120 Times")
-schaltf1 = tk.Button(window, text="Do it (:<", command=x.start)
+schaltf1 = tk.Button(window, text="Do it (:<", command=clickthread.start)
 credit = tk.Label(window, text="Made by Stoppedwumm-Studios")
 site = tk.Label(window, text="https://github.com/Stoppedwumm-Studios/spacebar/")
 opensite = tk.Button(window, text="Open Page (Only Windows)", command=OpenClick)
